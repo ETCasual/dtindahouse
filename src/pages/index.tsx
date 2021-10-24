36e7637a875be7bf724de2dba0232a52af57cdbe
ETCasual <casualorient@gmail.com>
2021-10-25 05:13:14 +0800

    Update Text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import Head from 'next/head'
 import YouTube from 'react-youtube'
 import ImageGallery from 'react-image-gallery'
+import { FaYoutube } from 'react-icons/fa'
+
+import { useRouter } from 'next/router'
 
 const images = [
   {
@@ -23,43 +26,71 @@ const images = [
 ]
 
 const Home: NextPage = () => {
+  const { push } = useRouter()
+
   return (
     <>
       <Head>
         <title>dtindahouse</title>
       </Head>
-      <div className="p-5 top-0 relative bg-[#c3b091] md:flex sticky z-10 shadow-xl">
+      <div className="p-5 top-0 bg-[#c3b091] md:flex fixed z-10 shadow-xl w-screen">
         <p className="font-script lg:text-5xl md:text-4xl text-3xl text-[#ffe4c4]">
           #dtindahouse
         </p>
         <p className="ml-5 font-script font-bold lg:text-5xl md:text-4xl text-3xl text-white transform hover:scale-[1.1] origin-left transition duration-500">
-          Dawn Hui & Kok Tim
+          Kok Tim & Dawn Hui
         </p>
       </div>
-      <div className="sm:flex sm:h-screen lg:h-full">
-        <ImageGallery
-          items={images}
-          infinite
-          autoPlay
-          showNav={false}
-          showThumbnails={false}
-          showPlayButton={false}
-          slideDuration={1000}
-          slideInterval={5000}
-          additionalClass="object-cover object-center sm:w-2/3 w-full h-full"
-        />
-        <div className="sm:w-1/3 w-full bg-[#fbf9f7]">
-          <div className="mx-10 mb-10 sm:m-15">
-            <p className="mt-5 font-passion text-2xl lg:text-3xl xl:text-4xl text-[#c3b091] w-full text-center">
-              SAVE THE DATE!
+      <div className="sm:flex h-full sm:h-screen w-full bg-[#fbf9f7] mt-[88px]">
+        <div className="relative sm:w-2/3 w-full">
+          <p className="font-script z-[5] text-white lg:text-3xl text-lg sm:text-xl absolute sm:fixed bottom-5 left-5 sm:left-10">
+            1 Corinthians 13:8 - Love never fails.
+          </p>
+          <ImageGallery
+            items={images}
+            infinite
+            autoPlay
+            showNav={false}
+            showThumbnails={false}
+            showPlayButton={false}
+            slideDuration={1000}
+            slideInterval={5000}
+            additionalClass="object-cover object-center sm:fixed sm:left-0 sm:h-screen h-full cursor-default bg-[#fbf9f7]"
+          />
+        </div>
+        <div className="sm:w-1/3 w-full bg-[#fbf9f7] absolute right-0">
+          <div className="mx-5 mb-10 sm:m-15">
+            <p className="mt-16 font-roboto text-lg tracking-wider lg:text-xl xl:text-2xl text-[#c3b091] w-full text-center">
+              TOGETHER WITH THEIR FAMILIES
             </p>
-
-            <p className="font-passion text-2xl md:text-3xl mt-3 w-full text-center">
-              Sunday, 7 October 2021, 10am
+            <p className="font-script text-6xl w-full text-center mt-5">
+              Kok Tim
             </p>
+            <p className="font-script text-6xl w-full text-center mt-3">&</p>
+            <p className="font-script text-6xl w-full text-center mb-5">
+              Dawn Hui
+            </p>
+            <p className="mt-10 font-roboto tracking-wider text-lg lg:text-xl xl:text-2xl text-[#c3b091] w-full text-center">
+              INVITE YOU TO CELEBRATE
+              <br />
+              THEIR WEDDING
+            </p>
+            <p className=" text-2xl md:text-3xl my-10 w-full text-center font-roboto">
+              Sunday | 7 October 2021 | 10am
+            </p>
+            <a
+              href="https://www.youtube.com/watch?v=ST17VRpVvdc"
+              target="_blank"
+              className="mt-10"
+            >
+              <div className="flex items-center justify-center">
+                <FaYoutube size={50} color="#FF0000" />
+                <p className="font-bold ml-2 text-2xl">YouTube</p>
+              </div>
+            </a>
           </div>
 
-          <div className="sm:m-10 lg:m-15">
+          <div className="sm:m-5 lg:m-10 mb-10 sm:mb-0">
             <p className="mt-5 font-passion text-2xl lg:text-3xl xl:text-4xl text-[#c3b091] w-full text-center">
               Join Us On Live!
             </p>
@@ -70,28 +101,32 @@ const Home: NextPage = () => {
             />
           </div>
 
-          <div className="sm:m-10 lg:m-15 sm:rounded-2xl bg-[#f7e7ce]">
+          <div className="sm:m-5 lg:m-10 sm:rounded-2xl bg-[#f7e7ce]">
             <p className="font-passion text-2xl lg:text-3xl xl:text-4xl text-[#c3b091] w-full text-center pt-2">
               Bless Us!
             </p>
-            <div className="flex justify-around py-5">
-              <div className="flex flex-col items-center">
+            <div className="flex flex-col py-5">
+              <div className="flex items-center mx-5 mb-2">
                 <img
                   src="https://play-lh.googleusercontent.com/sJ63Y_EFmkJuOeY7R9gJLjs9FAHYHxFxyuF0RJ0caALJJWL3srmdhuucyCjTnV-GpRg"
                   alt="tng"
                   className="lg:w-16 lg:h-16 sm:w-14 sm:h-14 w-20 h-20"
                 />
-                <p>Foong Dawn Hui</p>
-                <p>014-9709138</p>
+                <div className="ml-5">
+                  <p>Foong Dawn Hui</p>
+                  <p>014-9709138</p>
+                </div>
               </div>
-              <div className="flex flex-col items-center">
+              <div className="flex items-center mx-5">
                 <img
-                  src="https://www.nicepng.com/png/full/42-422061_credit-card-.png"
+                  src="pngegg.png"
                   alt="bankCard"
                   className="lg:w-16 lg:h-16 sm:w-14 sm:h-14 w-20 h-20"
                 />
-                <p>Wong Kok Tim - CIMB</p>
-                <p>7005779973</p>
+                <div className="ml-5">
+                  <p>Wong Kok Tim - CIMB</p>
+                  <p>7005779973</p>
+                </div>
               </div>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-    mode: 'jit',
-    // important: true,
-    purge: ['./src/**/*.tsx', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                script: ["'Dancing Script'", 'sans-serif'],
-                passion: ['"Passion One"', 'sans-serif'],
-            },
-        },
+  mode: 'jit',
+  // important: true,
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        script: ["'Dancing Script'", 'sans-serif'],
+        passion: ['"Passion One"', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio')],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')],
 }
